3ab355cdc574411b074c169621449b5f08caabb2
Rahul Vishwakarma <103241598+rahulvishwakarma0111@users.noreply.github.com>
2023-02-27 22:23:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,33 @@
-//your code here
-var randomNum;
-var input;
-function randomNumGenerator(){
-	randomNum = Math.floor(Math.random()*41) - 20;
-	document.getElementById("num").innerHTML = randomNum;
-	guessName();
-	print();
-}
-function guessName() {
-	input = parseInt(document.getElementById("guess").value);	
-}
-function print(){
-	if(Math.abs(randomNum - input) <= 5){
-		document.getElementById("respond").innerHTML = "Hot";
-	}
-	else{
-		document.getElementById("respond").innerHTML = "Cold";
-	}
-}
+var btn = document.getElementById("btn");
+        var numPtag = document.getElementById("num");
+        var res = document.getElementById("respond");
+        var inputValue = document.getElementById("guess").value;
 
+        function randomNumGenerator() {
+            
+            randomNum = Math.floor(Math.random()*41)-20;
+            document.getElementById("num").innerText = randomNum;
+
+            guessNum();
+            print();
+        };
+
+        function guessNum() {
+            
+            input = Number(document.getElementById("guess").value);
+        };
+
+        function print() {
+            
+            if (Math.abs(randomNum - input) <= 5) {
+                
+                document.getElementById("respond").innerText = "Hot";
+            }
+            else{
+
+                document.getElementById("respond").innerHTML = "cold";
+            }
+        };
+
+        btn.addEventListener("click", randomNumGenerator);
 
