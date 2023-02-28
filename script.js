91e1b1ebd8ca87f30563ebb87c4b9de822396dbb
Rahul Vishwakarma <103241598+rahulvishwakarma0111@users.noreply.github.com>
2023-02-28 11:42:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,33 +1,26 @@
-var btn = document.getElementById("btn");
-        var numPtag = document.getElementById("num");
-        var res = document.getElementById("respond");
-        var inputValue = document.getElementById("guess").value;
+//your code here
+var randomNum;
+var input;
 
-        function randomNumGenerator() {
-            
-            randomNum = Math.floor(Math.random()*41)-20;
-            document.getElementById("num").innerText = randomNum;
+function randomNumGenerator() {
+	randomNum = Math.floor(Math.random() * 41) - 20;
+	document.getElementById("num").innerText= randomNum;
+	guessNum();
+	print()
+	
+}
 
-            guessNum();
-            print();
-        };
+function guessNum() {
+	 
+	input=Number(document.getElementById("guess").value);
+	 
 
-        function guessNum() {
-            
-            input = Number(document.getElementById("guess").value);
-        };
-
-        function print() {
-            
-            if (Math.abs(randomNum - input) <= 5) {
-                
-                document.getElementById("respond").innerText = "Hot";
-            }
-            else{
-
-                document.getElementById("respond").innerHTML = "cold";
-            }
-        };
-
-        btn.addEventListener("click", randomNumGenerator);
-
+}
+function print() {
+	if (Math.abs( randomNum - input) <= 5){
+		document.getElementById("respond").innerText= "Hot";
+	}else{
+		document.getElementById("respond").innerHTML=  "Cold";
+		
+	}
+}
