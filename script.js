7fc07c14e99e9ececd36117f9c0f8541ef4fd8b7
Rahul Vishwakarma <103241598+rahulvishwakarma0111@users.noreply.github.com>
2023-02-28 11:55:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,23 @@
 //your code here
-var btn = document.getElementById("btn");
+var btn= document.getElementById("btn");
 var numPtag = document.getElementById("num");
-var res = document.getElementById("response");
+var res = document.getElementById("respond");
 var inputValue = document.getElementById("guess").value;
-
-
-
-
-function rendomNumGenerator(){
-	rendomNum = Math.floor(Math.random() * 40) - 20;
-	document.getElementById("num").innerText = randomNum;
+function randomNum() {
+	no = Math.floor(Math.random()*40 -20);
+	document.getElementById("num").innerText = no;
 	guessNum();
 	print();
 }
-function guessNum(){
-	input = number( documtent.getElementById("guess").value);
+function guessNum() {
+	input = Number(document.getElementById("guess").value);
 }
 function print() {
-	
-if(Math.abs(randomNum - input) <= 5){
-   document.getElementById("response").innerText = "Hot";
+	if(Math.abs(no-input) <=5){
+		document.getElementById("respond").innerText="Hot";
+	}
+	else{
+		document.getElementById("respond").innerText="Cold";
+	}
 }
-else(
-   document.getElementById("response").innerText = "Cold";
-)
-	
-}
-btn.addEventListener('click',rendomNumGenerator);
+btn.addEventListener("click" , randomNum);
